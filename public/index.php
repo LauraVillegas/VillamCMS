f07043ff04be9a4d19f4051a15a494c3e2032691
LauraVillegas <vils.in.town@gmail.com>
2018-03-21 01:35:57 -0700

    Created basic file structure and set up basic path functions.

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Villam Music</title>
+<meta charset="utf-8">	
+</head>
+<body>
+	<header>
+		<h1>Villam Music Coming Soon</h1>
+	</header>
+	
+	<navigation>
+		
+	</navigation>
+	
+	<footer>
+		
+	</footer>
+</body>
+</html>
